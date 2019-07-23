0dec3442c066d63d49f72661ed2992330cce84a9
Gavin Kendall <gkendall@igloosoftware.com>
2019-07-23 12:39:11 -0400

    1.0.0.0 - Initial commit. It's not very exciting except for a command prompt and the ability to exit by entering the "exit" command. Yay.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cyanspy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string commandInput = string.Empty;
+
+            do
+            {
+                Console.Write("> ");
+                commandInput = Console.ReadLine();
+            }
+            while (!commandInput.Equals("exit"));
+        }
+    }
+}
