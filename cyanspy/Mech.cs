a271a38b7c848c45c41a57bc4ed046a832dd4396
gavinkendall <gavinkendall@gmail.com>
2019-11-17 14:56:43 -0500

    Fixed coordinates on map to be Cartesian

---
+++ cyanspy/Mech.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cyanspy
+{
+    public class Mech
+    {
+        // A mech has pilots, applications, components, and weapons.
+        public Dictionary<string, Pilot> Pilots {get; set; }
+
+        public string Name { get; set; }
+        public string Mnemonic { get; private set; }
+
+        public Mech(string name)
+        {
+            Name = name;
+            Mnemonic = "M";
+            Pilots = new Dictionary<string, Pilot>();
+        }
+    }
+}
